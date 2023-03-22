95b28a783eaa67b4dabbf85a4073bd5f1262b66e
Cesar Augusto M. C. da Silva <caugustomendes653@gmail.com>
2023-03-21 21:56:52 -0300

    Addo codigo

---
+++ Aula_1(21_03_23)/DHT/main.py
@@ -0,0 +1,29 @@
+'''
+ * Docente:          Leopoldo André Dutra Lusquino Filho
+ * Discente:         Cesar Augusto Mendes Cordeiro da Silva
+  * GitHub:          https://github.com/Cesarquatro
+ * Simulador online: https://wokwi.com/projects/359809346682295297
+ * Aula 1 - DHT
+ * 21/03/2023
+'''
+#Importação de bibliotecas
+import dht            #sensor de umidade e temperatura
+import machine as mc  #microcontroladores
+import time           #manipulação de tempo (delay)
+
+#Define a porta e o modo do pino como saída
+'''
+No arduino:
+#define led_pin 25;
+pinMode(led_pin, OUTPUT);
+
+'''
+dht_pin = mc.Pin(4, mc.Pin.IN)
+dht_sensor = dht.DHT11(dht_pin)
+
+while True:
+    dht_sensor.measure() #Realiza a medida
+    temperature = dht_sensor.temperature() #Armazena a medida da temperatura na variavel
+    humidity = dht_sensor.humidity()       #Armazena a medida da umidade na variavel
+    print(f"Temp: {temperature}ºC\tHum: {humidity}%") #print os valores
+    time.sleep(2) #espera 2[s]
